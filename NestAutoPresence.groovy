82861659bda394ed069baffbe5d0e2bc123089a5
Gus Perez <gus@gusperez.com>
2015-02-15 11:49:49 -0800

    Updated to now set Presence 5 minutes later
    Since someone's presence is usually noticed by the system before they're actually at the location, I've delayed it by five minutes to keep the away state on (and therefore my dropcams) to make sure I also record my dog's behavior right when I actually arrive and enter our home.

--- NestAutoPresence.groovy
+++ NestAutoPresence.groovy
@@ -4,20 +4,20 @@
  *  Author: Gus Perez
  */
 definition(
-    name: "Toggle Nest Home/Away based on my presence",
+    name: "NestAutoPresence",
     namespace: "gusper",
     author: "Gus Perez",
-    description: "Set Nest to Home when you arrive and to Away when you leave.",
+    description: "Set Nest's presence state to Home when you arrive and to Away when you leave.",
     category: "Convenience",
     iconUrl: "https://s3.amazonaws.com/smartapp-icons/Categories/presence.png",
     iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Categories/presence@2x.png"
 )
 
 preferences {
-	section("When I arrive and leave..."){
+	section("When I arrive and leave...") {
 		input "presence1", "capability.presenceSensor", title: "Who?", multiple: true
 	}
-	section("Set Nest to home/away..."){
+	section("Set Nest to home/away...") {
 		input "nest1", "capability.presenceSensor", multiple: false
 	}
 }
@@ -38,14 +38,22 @@ def presenceHandler(evt)
 	log.debug "presenceHandler $evt.name: $evt.value"
 	def current = presence1.currentValue("presence")
 	log.debug current
-	def presenceValue = presence1.find{it.currentPresence == "present"}
+	def presenceValue = presence1.find { it.currentPresence == "present" }
 	log.debug presenceValue
-	if(presenceValue){
-		nest1.present()
+	
+    if (presenceValue) {
+		// Run 5 minutes after presence is noticed as it usually notices
+        // a few minutes before you actually arrive at your location.
+		runIn(300, setNestToPresent)
 		log.debug "Someone's home!"
 	}
-	else{
+	else {
 		nest1.away()
 		log.debug "Everyone's away."
 	}
+}
+
+def setNestToPresent()
+{
+	nest1.present()
 }
